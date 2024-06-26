9f65733516f959925335efd7f3978fa752b7300c
abhishekmg <abhishekmg.12@gmail.com>
2024-06-26 19:30:36 +0530

    testing

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,5 +1,19 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { pipeline } from "@xenova/transformers";
 
-export default function handler(req, res) {
-  res.status(200).json({ name: "John Doe" });
+const imageToText = async (url) => {
+  const captioner = await pipeline(
+    "image-to-text",
+    "Xenova/vit-gpt2-image-captioning"
+  );
+  const output = await captioner(url);
+
+  return output?.[0]?.generated_text;
+};
+
+export default async function handler(req, res) {
+  const ans = await imageToText(
+    "https://pbs.twimg.com/media/GOrHYdiX0AAmn4J.jpg"
+  );
+
+  res.status(200).json({ name: ans });
 }
